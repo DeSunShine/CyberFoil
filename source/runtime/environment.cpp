34a05bbb15316805843f5f52303483978aa27bfb
DeSunShine <87091315+DeSunShine@users.noreply.github.com>
2026-08-28 02:29:32 +0300

    fix: use libnx SPL config API correctly

--- source/runtime/environment.cpp
+++ source/runtime/environment.cpp
@@ -4,10 +4,14 @@
 namespace cyberfoil::runtime {
 
 Environment DetectEnvironment() {
-    bool isEmu = false;
-    const Result rc = splGetConfig(SplConfigItem(65007), &isEmu, sizeof(isEmu));
+    if (R_FAILED(splInitialize())) return Environment::Unknown;
+
+    u64 isEmu = 0;
+    const Result rc = splGetConfig(SplConfigItem(65007), &isEmu);
+    splExit();
+
     if (R_FAILED(rc)) return Environment::Unknown;
-    return isEmu ? Environment::EmuMMC : Environment::SysMMC;
+    return isEmu != 0 ? Environment::EmuMMC : Environment::SysMMC;
 }
 
 bool IsEmuMMC() {
